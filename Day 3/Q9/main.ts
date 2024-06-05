d9a6fff032200428d8be130c2e3b6d4c0b090a23
hasan <hasan@coderhasan.com>
2024-06-05 22:20:58 +0500

    first commit

---
+++ Day 3/Q9/main.ts
@@ -0,0 +1,7 @@
+// Question 9:  Favorite Number: Store your favorite number in a variable.
+//  Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favoriteNum = 15;
+let message = "My favourite number is "+favoriteNum;
+
+console.log(message);
